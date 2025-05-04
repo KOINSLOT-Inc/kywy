1bc273d481d87c420e590c5c76c47e421584761b
jmlannan <jmlannan@gmail.com>
2025-05-04 18:35:37 -0400

    removed extra exports

--- examples/utility/Template/Template.ino
+++ examples/utility/Template/Template.ino
@@ -2,27 +2,13 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include <Actor.hpp>
-#include <Battery.hpp>
-#include <Clock.hpp>
-#include <Display.hpp>
-#include <Events.hpp>
-#include <Font.hpp>
-#include <Fonts.hpp>
-#include <GraphicsObject.hpp>
-#include <Input.hpp>
-#include <Kywy.hpp>
-#include <Sprite.hpp>
-#include <SpriteSheet.hpp>
-#include <StateMachine.hpp>
-
 #include "Kywy.hpp"
-#include <pico/bootrom.h>
 
 Kywy::Engine engine;
 
 void setup() {
   engine.start();
+  
 }
 
 void loop() {
