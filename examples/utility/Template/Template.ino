cae73c22ff53e43bd144399dc64da4b26ec41532
Joe <3395054+Joe-Lannan@users.noreply.github.com>
2025-05-07 10:04:59 -0400

    Hello World and empty Template Examples (#37)
    ---------
    
    Co-authored-by: Erik <erik@kywy.io>

---
+++ examples/utility/Template/Template.ino
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2025 KOINSLOT, Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include "Kywy.hpp"
+
+Kywy::Engine engine;
+
+void setup() {
+  engine.start();
+}
+
+void loop() {
+}
