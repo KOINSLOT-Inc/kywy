1895138c212e99491cdf4510c837c9a054d096e1
Dave <dcalvert2014@gmail.com>
2025-09-04 21:36:59 -0700

    Added licensing

--- examples/games/Adventure/screens.hpp
+++ examples/games/Adventure/screens.hpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 KOINSLOT, Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // ================================================
 //             my_image BITMAP ARRAY START
 // ================================================
