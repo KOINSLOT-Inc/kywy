d84b704ee68bd0011ce3fbee845778392d04e0bf
Joe <jmlannan@ncsu.edu>
2025-09-13 22:56:13 -0400

    working maze example code

---
+++ examples/games/Maze/finish_flag.hpp
@@ -0,0 +1,10 @@
+// Generated bitmap data for 8x8 image
+// Created with Kywy Drawing Editor
+
+uint8_t finish_flag_data[8] = {
+    0xBF, 0xAA, 0x95, 0xAA, 0x95, 0xAA, 0xBF, 0xBF
+};
+
+// Bitmap Constants
+#define FINISH_FLAG_WIDTH 8
+#define FINISH_FLAG_HEIGHT 8
---
+++ examples/games/Maze/start_flag.hpp
@@ -0,0 +1,10 @@
+// Generated bitmap data for 8x8 image
+// Created with Kywy Drawing Editor
+
+uint8_t start_flag_data[8] = {
+    0xBF, 0x80, 0x80, 0x80, 0x80, 0x80, 0xBF, 0xBF
+};
+
+// Bitmap Constants
+#define START_FLAG_WIDTH 8
+#define START_FLAG_HEIGHT 8
