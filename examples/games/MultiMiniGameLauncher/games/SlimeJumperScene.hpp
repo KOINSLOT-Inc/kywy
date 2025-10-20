e915cd7e97b32562e5d3190eed72639af7b54f0c
Joe <jmlannan@ncsu.edu>
2025-10-20 14:52:56 -0400

    Prevent slimejumper from drawing over menu

--- examples/games/MultiMiniGameLauncher/games/SlimeJumperScene.hpp
+++ examples/games/MultiMiniGameLauncher/games/SlimeJumperScene.hpp
@@ -78,6 +78,7 @@ private:
     }
 
     void handle(::Actor::Message *message) {
+      if(!scene->isActive()) return;
       switch (message->signal) {
         case Kywy::Events::D_PAD_LEFT_PRESSED:
           buttonLeftPressed = true;
@@ -184,6 +185,7 @@ private:
     }
 
     void handle(::Actor::Message *message) {
+      if(!scene->isActive()) return;
       switch (message->signal) {
         case Kywy::Events::TICK:
           if (scene->gameManager.getCurrentState() != STATE_GAME_ACTIVE) break;
@@ -253,6 +255,7 @@ private:
     }
 
     void drawScore(uint16_t color) {
+      if(!scene->isActive()) return;
       char msg[16];
       snprintf(msg, sizeof(msg), "%d", (uint16_t)score);
       scene->Scene::getEngine()->display.drawText(5, 5, msg, Display::TextOptions().color(color));
