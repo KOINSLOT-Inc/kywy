5e5ea2b9606f1b0278eff689b0af38ba3ba72ac7
Joe <jmlannan@ncsu.edu>
2025-11-03 18:52:29 -0500

    Better comments on Template.ino

--- examples/utility/Template/Template.ino
+++ examples/utility/Template/Template.ino
@@ -2,59 +2,89 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+
+// =================================================================== //
+//
+//                      Name:  Your Name
+//                      Date:  Today
+//                   Project:  Your Project Name
+//               Description:  Your project description
+//
+// =================================================================== //
+
+// Get started and more at https://www.kywy.io
+// For help, see the docs: https://docs.kywy.io/
+// For tools like the drawing editor and more see: https://tools.kywy.io/
+
 #include "Kywy.hpp"
 
 Kywy::Engine engine;
 
 void setup() {
+  // Run once at startup. Initialize the engine and your application here
   engine.clock.setTickDuration(30);  // Set time between ticks
   engine.start();
-}
+}  // end setup
 
 void loop() {
-}
+  // Run continuously (no delay), usually not needed if you use onTick()
+}  // end loop
 
 void onTick(Actor::Message *message) {
-  // Do stuff every clock tick i.e. draw to display
-}
+  // Run every clock tick i.e. draw to display
+  // See display functions here: https://docs.kywy.io/api_reference/
+}  // end onTick
 
 void onInput(Actor::Message *message) {
   // Do stuff when buttons are pressed/released
   switch (message->signal) {
-    case Kywy::Events::BUTTON_RIGHT_PRESSED:
+    case Kywy::Events::BUTTON_LEFT_PRESSED:    //  🟢 (LEFT PRESSED)
+      //  Handle Left button press here
       break;
 
-    case Kywy::Events::BUTTON_LEFT_PRESSED:
+    case Kywy::Events::BUTTON_RIGHT_PRESSED:   //  🟢 (RIGHT PRESSED)
+      //  Handle Right button press here
       break;
 
-    case Kywy::Events::D_PAD_LEFT_PRESSED:
+    case Kywy::Events::D_PAD_LEFT_PRESSED:     //  ◀️ (D-PAD LEFT PRESSED)
+      //  Handle D-Pad Left press here
       break;
 
-    case Kywy::Events::D_PAD_RIGHT_PRESSED:
+    case Kywy::Events::D_PAD_RIGHT_PRESSED:    //  ▶️ (D-PAD RIGHT PRESSED)
+      //  Handle D-Pad Right press here
       break;
 
-    case Kywy::Events::D_PAD_UP_PRESSED:
+    case Kywy::Events::D_PAD_UP_PRESSED:       //  🔼 (D-PAD UP PRESSED)
+      //  Handle D-Pad Up press here
       break;
 
-    case Kywy::Events::D_PAD_DOWN_PRESSED:
+    case Kywy::Events::D_PAD_DOWN_PRESSED:     //  🔽 (D-PAD DOWN PRESSED)
+      //  Handle D-Pad Down press here
       break;
 
-    case Kywy::Events::BUTTON_RIGHT_RELEASED:
+    case Kywy::Events::BUTTON_LEFT_RELEASED:   //  🔴 (LEFT RELEASED)
+      //  Handle Left button release here
       break;
 
-    case Kywy::Events::BUTTON_LEFT_RELEASED:
+    case Kywy::Events::BUTTON_RIGHT_RELEASED:  //  🔴 (RIGHT RELEASED)
+      //  Handle Right button release here
       break;
 
-    case Kywy::Events::D_PAD_LEFT_RELEASED:
+    case Kywy::Events::D_PAD_LEFT_RELEASED:    //  ⬅️ (D-PAD LEFT RELEASED)
+      //  Handle D-Pad Left release here
       break;
 
-    case Kywy::Events::D_PAD_RIGHT_RELEASED:
+    case Kywy::Events::D_PAD_RIGHT_RELEASED:   //  ➡️ (D-PAD RIGHT RELEASED)
+      //  Handle D-Pad Right release here
       break;
 
-    case Kywy::Events::D_PAD_UP_RELEASED:
+    case Kywy::Events::D_PAD_UP_RELEASED:      //  ⬆️ (D-PAD UP RELEASED)
+      //  Handle D-Pad Up release here
       break;
 
-    case Kywy::Events::D_PAD_DOWN_RELEASED:
+    case Kywy::Events::D_PAD_DOWN_RELEASED:    //  ⬇️ (D-PAD DOWN RELEASED)
+      //  Handle D-Pad Down release here
       break;
-  }
-}
+
+  }  // end switch message->signal
+}  // end onInput
