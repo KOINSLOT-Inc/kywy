2fe2df07d450a9fe2125960767658d5c1863c1f7
Joe <jmlannan@ncsu.edu>
2025-11-04 19:09:39 -0500

    Update RenderAnimation to use onTick()

--- examples/basics/RenderAnimation/RenderAnimation.ino
+++ examples/basics/RenderAnimation/RenderAnimation.ino
@@ -14,11 +14,12 @@ int centerX = (144 - MY_ANIMATION_WIDTH) / 2;   // 144 is display width
 int centerY = (168 - MY_ANIMATION_HEIGHT) / 2;  // 168 is display height
 
 void setup() {
+  engine.clock.setTickDuration(500);  // 500 ms per tick/frame
   engine.start();
 }
 
-void loop() {
-  // Clear the display to get rid of the last frame
+void onTick(Actor::Message *message) {
+    // Clear the display to get rid of the last frame
   engine.display.clear();
 
   // Draw the current frame
@@ -29,7 +30,8 @@ void loop() {
 
   // Advance to next frame
   currentFrame = (currentFrame + 1) % MY_ANIMATION_FRAME_COUNT;
+}
 
-  // Wait for the next frame
-  delay(MY_ANIMATION_SPEED);
+void loop() {
+  // Nothing to do here since animation is handled in onTick
 }
